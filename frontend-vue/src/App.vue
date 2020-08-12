99826b5a46bf9b2b2641d2e79d68a23ecdaab512
jteaford <jessteaford@gmail.com>
2020-08-12 15:27:50 -0500

    Initial Commit

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/clients">Clients</router-link> |
-      <router-link to="/projects">Projects</router-link>
+      <router-link to="/">Home</router-link> <span>|</span>
+      <router-link to="/clients">Clients</router-link> <span>|</span>
+      <router-link to="/projects">Projects</router-link> <span>|</span>
+      <router-link to="/ledger">Business Ledger</router-link>
     </div>
     <div class="container">
     <router-view/>
@@ -21,15 +22,26 @@
 }
 
 #nav {
-  padding: 30px;
+  padding: 30px 30px 30px 100px;
+  display: flex;
+  background-color: black; 
+  margin-bottom: 10vh;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  font-weight: lighter;
+  color: white;
+  padding-right: 5vw;
+}
+
+#nav span {
+  padding-right: 5vw;
+  color: white;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #666666;
 }
+
+ 
 </style>

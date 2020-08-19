c33637ee1d1ac7744235cd889f0bf0a15c5a5476
jteaford <jessteaford@gmail.com>
2020-08-18 22:32:23 -0500

    Initial Commit

--- backend/src/main/java/org/aim/carpentry/status/StatusController.java
+++ backend/src/main/java/org/aim/carpentry/status/StatusController.java
@@ -1,5 +1,6 @@
 package org.aim.carpentry.status;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
